20baf33aaaddf606e3b90bcd107df45680bbd2db
James Benjamin <james.abenj@gmail.com>
2019-10-18 02:03:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,11 +28,11 @@ class Owner
   end 
   
   def cats 
-    Cat.all.select {|cats| cats.owner.name == self.name}
+    Cat.all.select {|cat| cat.owner.name == self.name}
   end 
   
   def dogs 
-    Dog.all.map {|dogs| dogs.owner.name == self.name}
+    Dog.all.map {|dog| dog.owner.name == self.name}
     #binding.pry 
   end 
   
@@ -40,13 +40,23 @@ class Owner
     Cat.new(cat, self)
     #binding.pry
   end 
+  
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end 
+  
+  def walk_dogs
+  end 
+  
+  def feed_cats
+  end 
+  
+  def sell_pets
+  end 
+  
+  def list_pets
     
-    
-  
-
-  
-
-  
+  end 
  
   
 end
